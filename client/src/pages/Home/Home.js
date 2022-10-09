e08ad54cdb8b3e1b7b83cede42b5094f1ea7d191
Max Healy <maxhealy01@gmail.com>
2022-10-09 13:31:36 -0500

    Home separated from MapPage concerns.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,9 +3,8 @@ import { useQuery } from "@apollo/react-hooks";
 import { QUERY_NOTES } from "../../utils/queries";
 
 import SplashPage from "../SplashPage/SplashPage";
-import WrappedMap from "../../components/Map/Map";
-import NoteForm from "../../components/NoteForm/NoteForm";
-import SelectedNote from "../../components/SelectedNote/SelectedNote";
+import MapPage from "../MapPage/MapPage";
+
 import Auth from "../../utils/auth";
 import { exampleNotes } from "../../utils/exampleNotes";
 
@@ -16,14 +15,6 @@ const Home = () => {
 	const [longitude, setLongitude] = useState("");
 
 	const [selectedNote, setSelectedNote] = useState("");
-
-	// Query notes and then make them stateful in order to update map on note submission
-	const [notes, setNotes] = useState([]);
-	const { error, data } = useQuery(QUERY_NOTES, {
-		onCompleted: () => {
-			setNotes(data.notes.concat(exampleNotes));
-		},
-	});
 
 	// Create logic for the button that asks for a user's location
 	const [locationKnown, setLocationKnown] = useState(false);
@@ -45,42 +36,30 @@ const Home = () => {
 		});
 	}
 
+	// Query notes and then make them stateful in order to update map on note submission
+	// This occurs after Lat + Long are set so that the exampleNotes can be placed
+	// in visible locations
+	const [notes, setNotes] = useState([]);
+	const { error, data } = useQuery(QUERY_NOTES, {
+		onCompleted: () => {
+			setNotes(data.notes.concat(exampleNotes));
+		},
+	});
+
 	return (
 		<div>
-			{!Auth.loggedIn() && !locationKnown && (
+			{!Auth.loggedIn() && !locationKnown ? (
 				<SplashPage handleLocation={handleLocation} />
+			) : (
+				<MapPage
+					setSelectedNote={setSelectedNote}
+					selectedNote={selectedNote}
+					setNotes={setNotes}
+					notes={notes}
+					latitude={latitude}
+					longitude={longitude}
+				/>
 			)}
-			{latitude && data && (
-				<div className="map">
-					<WrappedMap
-						setSelectedNote={setSelectedNote}
-						notes={notes}
-						lat={latitude}
-						long={longitude}
-						isMarkerShown
-						googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAE5FDeD7mf02RHDXEu2SzZWlrWJCbwSw"
-						loadingElement={<div style={{ height: `100%` }} />}
-						containerElement={<div style={{ height: `400px` }} />}
-						mapElement={<div style={{ height: `100%` }} />}
-					/>
-				</div>
-			)}
-			<div className="notesOrComments">
-				{latitude && notes && !selectedNote && (
-					<NoteForm
-						lat={latitude}
-						long={longitude}
-						setNotes={setNotes}
-						notes={notes}
-					/>
-				)}
-				{selectedNote && (
-					<SelectedNote
-						selectedNote={selectedNote}
-						setSelectedNote={setSelectedNote}
-					/>
-				)}
-			</div>
 			<script
 				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAE5FDeD7mf02RHDXEu2SzZWlrWJCbwSw&callback=initMap&v=weekly"
 				defer
---
+++ client/src/pages/MapPage/MapPage.js
@@ -0,0 +1,46 @@
+import React from "react";
+import WrappedMap from "../../components/Map/Map";
+import NoteForm from "../../components/NoteForm/NoteForm";
+import SelectedNote from "../../components/SelectedNote/SelectedNote";
+
+const MapPage = ({
+	setSelectedNote,
+	selectedNote,
+	setNotes,
+	notes,
+	latitude,
+	longitude,
+}) => {
+	return (
+		<div className="map">
+			<WrappedMap
+				setSelectedNote={setSelectedNote}
+				notes={notes}
+				lat={latitude}
+				long={longitude}
+				isMarkerShown
+				googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAE5FDeD7mf02RHDXEu2SzZWlrWJCbwSw"
+				loadingElement={<div style={{ height: `100%` }} />}
+				containerElement={<div style={{ height: `400px` }} />}
+				mapElement={<div style={{ height: `100%` }} />}
+			/>
+			<div className="notesOrComments">
+				{selectedNote ? (
+					<SelectedNote
+						selectedNote={selectedNote}
+						setSelectedNote={setSelectedNote}
+					/>
+				) : (
+					<NoteForm
+						lat={latitude}
+						long={longitude}
+						setNotes={setNotes}
+						notes={notes}
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default MapPage;
